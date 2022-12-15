6303d38291e2c138e05214f717ab3677ed179dd0
Beenish  Butt <beenish.butt@and.digital>
2022-12-15 13:51:21 +0000

    make the genral background put some styling and get the api on click handler

--- app/components/joke.js
+++ app/components/joke.js
@@ -1,23 +1,21 @@
 import Component from '@glimmer/component';
-import { trackedFunction } from 'ember-resources/util/function';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 
 export default class JokeComponent extends Component {
-  @tracked fetchItems = '';
+  @tracked delivery = '';
+  @tracked setup = '';
 
   @action
-  getJoke() {
-    this.fetchItems = trackedFunction(this, async () => {
-      let response = await fetch(
-        'https://v2.jokeapi.dev/joke/Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
-      );
-      const data = await response.json();
-      return data;
-    });
-    // specify a default value of empty array when value is undefined
-    // (value is undefined before the function finishes running)
-    console.log(this.fetchItems);
-    return this.fetchItems.value ?? [];
+  async getJoke() {
+    let response = await fetch(
+      'https://v2.jokeapi.dev/joke/Christmas?blacklistFlags=nsfw,religious,political,racist,sexist,explicit'
+    );
+    const data = await response.json();
+    this.delivery = data.delivery;
+    this.setup = data.setup;
+
+    console.log('hi bee', data.delivery);
+    return data;
   }
 }
