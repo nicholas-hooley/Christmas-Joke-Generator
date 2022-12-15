f625d4e88194e3a790ac4a4a8eff01195e8c1192
My Name <nicholashooley27@yahoo.co.uk>
2022-12-15 15:07:38 +0000

    unit test in progress

--- tests/integration/components/joke-test.js
+++ tests/integration/components/joke-test.js
@@ -1,26 +1,25 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'christmas-joke-generator/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { click, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | joke', function (hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function (assert) {
+  test('it should not render a joke when button is clicked', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
+    assert.expect(2);
+
     await render(hbs`<Joke />`);
 
-    assert.dom(this.element).hasText('');
+    assert
+      .dom(this.element.querySelector('h1').textContent.trim())
+      .doesNotContainText();
 
-    // Template block usage:
-    await render(hbs`
-      <Joke>
-        template block text
-      </Joke>
-    `);
+    await click('title-button');
 
-    assert.dom(this.element).hasText('template block text');
+    assert.dom(this.element.querySelector('h1')).containsText();
   });
 });
